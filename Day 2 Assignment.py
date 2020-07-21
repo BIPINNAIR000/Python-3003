e099b5d3442221c85aa5371c768f92a1463b99cc
BIPINNAIR000 <68552023+BIPINNAIR000@users.noreply.github.com>
2020-07-21 19:56:58 +0530

    Add files via upload

---
+++ Day 2 Assignment.py
@@ -0,0 +1,281 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World")
+
+
+# In[3]:
+
+
+print ("Hello  World")
+
+
+# In[4]:
+
+
+print("""(\_/)
+( •,•)
+(")_(")""")
+
+
+# In[5]:
+
+
+"""(\_/)
+( •,•)
+(")_(")"""
+
+
+# In[6]:
+
+
+print('Hello World')
+
+
+# In[7]:
+
+
+print("Hello World's")
+
+
+# In[8]:
+
+
+print("Hello\tWorld")
+
+
+# In[10]:
+
+
+print("Hello\nWorld")
+
+
+# In[13]:
+
+
+print("Hello\aWorld")
+
+
+# In[14]:
+
+
+print('Hello World\'s')
+
+
+# In[15]:
+
+
+name="ABC"
+marks="95"
+age="20"
+print("The name of person is",name,"marks is",marks,"age is",age)
+
+
+# In[22]:
+
+
+name="ABC"
+marks=94.986
+age=20
+print("The name of person is",name,"marks is",marks,"age is",age)
+
+
+# In[25]:
+
+
+name="ABC"
+marks=94.986
+age=20
+print("The name of pearson is %10s marks is %10.2f age is %10d"%(name,marks,age))
+
+
+# In[26]:
+
+
+print(f"The name of person is{name} the marks is{94.99} age is {age}")
+
+
+# In[28]:
+
+
+a=10
+b=10
+a=b=10
+id(a)
+
+
+# In[29]:
+
+
+id(b)
+
+
+# In[30]:
+
+
+25**2
+
+
+# In[31]:
+
+
+25*2
+
+
+# In[32]:
+
+
+25/2
+
+
+# In[33]:
+
+
+25%2
+
+
+# In[34]:
+
+
+25+20
+
+
+# In[35]:
+
+
+25-20
+
+
+# In[36]:
+
+
+25//2
+
+
+# In[37]:
+
+
+a=10
+b=20
+a==b
+
+
+# In[38]:
+
+
+c=10
+a==c
+
+
+# In[39]:
+
+
+c<=a
+
+
+# In[40]:
+
+
+a>=b
+
+
+# In[41]:
+
+
+a!=b
+
+
+# In[42]:
+
+
+a=1
+b=0
+a|b
+
+
+# In[43]:
+
+
+a=1
+b=0
+a&b
+
+
+# In[44]:
+
+
+a=1
+b=0
+a^b
+
+
+# In[45]:
+
+
+a=1
+b=0
+a<<b
+
+
+# In[46]:
+
+
+a=1
+b=0
+a>>b
+
+
+# In[47]:
+
+
+str1="HelloWorld"
+"b" in str1
+
+
+# In[48]:
+
+
+str1="HelloWorld"
+"o" in str1
+
+
+# In[49]:
+
+
+a=10
+b=10
+a==b
+
+
+# In[50]:
+
+
+a is b
+
+
+# In[51]:
+
+
+a is not b
+
+
+# In[52]:
+
+
+20/40*5
+
+
+# In[53]:
+
+
+5**-1
+
+
+# In[ ]:
+
+
+
+
