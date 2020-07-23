7a1565495d83b293abb324e5eb0802a44901ac65
BIPINNAIR000 <68552023+BIPINNAIR000@users.noreply.github.com>
2020-07-23 19:53:47 +0530

    Add files via upload

---
+++ Day 4 Assignment.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Finding all occurences of substring in a given string
+# 
+
+# In[1]:
+
+
+str1="Where there is will there is a way"
+
+
+# In[5]:
+
+
+str1
+
+
+# In[4]:
+
+
+str1.count("there")
+
+
+# In[1]:
+
+
+str1="Where there is will there is a way"
+str1.find("there")
+
+
+# In[2]:
+
+
+str1[6]
+
+
+# In[3]:
+
+
+str1.rfind("there")
+
+
+# In[8]:
+
+
+str1.count("is")
+
+
+# In[9]:
+
+
+str1.find("is")
+
+
+# In[10]:
+
+
+str1[12]
+
+
+# # To use islower() and isupper() with different kind of String 
+
+# In[12]:
+
+
+str1="bipin"
+
+
+# In[13]:
+
+
+str1
+
+
+# In[14]:
+
+
+str1.islower()
+
+
+# In[15]:
+
+
+str1.isupper()
+
+
+# In[16]:
+
+
+str2="BIPIN"
+
+
+# In[17]:
+
+
+str2
+
+
+# In[18]:
+
+
+str2.isupper()
+
+
+# In[19]:
+
+
+str2.islower()
+
+
+# In[ ]:
+
+
+
+
